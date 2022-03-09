efc9b332c62b2b43caa90862c30f5189a823aa4e
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-09 11:30:52 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -27,12 +27,13 @@ class StoryMenuState extends MusicBeatState
 	var weekData:Array<Dynamic> = [
 		['Tutorial'],
 		['Bopeebo', 'Fresh', 'Dad Battle'],
+		['Nightshift-Hard', 'Flashinglights-Hard', 'Shadows-Hard'],
 		['Spookeez', 'South', "Monster"],
 		['Pico', 'Philly Nice', "Blammed"],
 		['Satin Panties', "High", "Milf"],
 		['Cocoa', 'Eggnog', 'Winter Horrorland'],
 		['Senpai', 'Roses', 'Thorns']
-		['Nightshift-Hard', 'Flashinglights-Hard', 'Shadows-Hard'],
+
 	];
 	var curDifficulty:Int = 1;
 
