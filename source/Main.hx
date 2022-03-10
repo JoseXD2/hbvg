fb1ec2eb94153872b2fbe3085fd6996f472a9823
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-10 11:34:40 -0300

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -17,9 +17,13 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = WarningState; // The FlxState the game starts with. //CHANGE THIS TO BIRDY MENU LOL
+	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 120; // How many frames per second the game should run at.
+	#if mobileC
+	var framerate:Int = 60; // How many frames per second the game should run at.
+	#else
+	var framerate:Int = 120;
+	#end
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
@@ -49,6 +53,8 @@ class Main extends Sprite
 		}
 	}
 
+	// public static var webmHandler:WebmHandler;
+
 	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
@@ -73,20 +79,17 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = WarningState; //CHANGE THIS TO BIRDY MENU LOL
+		#if mobile
+		gameWidth = 1280;
+		gameHeight = 720;
+		zoom = 1;
 		#end
-
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-
 		addChild(game);
-
-		#if !mobile
+		
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-
-		#end
 	}
 
 	var game:FlxGame;
